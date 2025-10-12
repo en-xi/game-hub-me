5327aa05b396acf5079e689373748297f63fec1a
hassan <enxi666freedom@gmail.com>
2025-10-12 21:41:29 +0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 let target = "media/";
 function getCropImageUrl(url: string) {
-  if (!url) return "";
+  if (!url) return noImage;
   let index = url.indexOf(target) + target.length;
   let newUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
   return newUrl;
