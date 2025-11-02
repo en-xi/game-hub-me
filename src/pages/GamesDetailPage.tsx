8ea80b4527eb3fcc47cbbdd27717848382faaf8c
hassan <enxi666freedom@gmail.com>
2025-11-02 18:28:45 +0800

    Exercise - Improving the Layout

--- src/pages/GamesDetailPage.tsx
+++ src/pages/GamesDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import CriticScore from "../components/CriticScore";
 import DefinitionItem from "../components/DefinitionItem";
@@ -16,32 +16,37 @@ const GamesDetailPage = () => {
 
   return (
     <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <Box>
+          <Heading>{game.name}</Heading>
+          <ExpandableText>{game.description_raw}</ExpandableText>
 
-      <SimpleGrid columns={2} as={"dl"}>
-        <DefinitionItem term="Platforms">
-          {game.parent_platforms.map(({ platform }) => (
-            <Text key={platform.id}>{platform.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term="Score">
-          <CriticScore score={game.metacritic} />
-        </DefinitionItem>
-        <DefinitionItem term="Genres">
-          {game.genres.map((genre) => (
-            <Text key={genre.id}>{genre.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term="Publishers">
-          {game.publishers.map((publisher) => (
-            <Text key={publisher.id}>{publisher.name}</Text>
-          ))}
-        </DefinitionItem>
+          <SimpleGrid columns={2} as={"dl"}>
+            <DefinitionItem term="Platforms">
+              {game.parent_platforms.map(({ platform }) => (
+                <Text key={platform.id}>{platform.name}</Text>
+              ))}
+            </DefinitionItem>
+            <DefinitionItem term="Score">
+              <CriticScore score={game.metacritic} />
+            </DefinitionItem>
+            <DefinitionItem term="Genres">
+              {game.genres.map((genre) => (
+                <Text key={genre.id}>{genre.name}</Text>
+              ))}
+            </DefinitionItem>
+            <DefinitionItem term="Publishers">
+              {game.publishers.map((publisher) => (
+                <Text key={publisher.id}>{publisher.name}</Text>
+              ))}
+            </DefinitionItem>
+          </SimpleGrid>
+        </Box>
+        <Box>
+          <GameTrailer gameId={game.id} />
+          <GameScreenshots gameId={game.id} />
+        </Box>
       </SimpleGrid>
-
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
     </>
   );
 };
