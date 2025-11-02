346be29ad874925d6f295ca493751edf4f3d7cc3
hassan <enxi666freedom@gmail.com>
2025-11-02 15:53:00 +0800

    Exercise - Building Game Attributes

--- src/pages/GamesDetailPage.tsx
+++ src/pages/GamesDetailPage.tsx
@@ -1,5 +1,7 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import CriticScore from "../components/CriticScore";
+import DefinitionItem from "../components/DefinitionItem";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 
@@ -14,6 +16,26 @@ const GamesDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <SimpleGrid columns={2} as={"dl"}>
+        <DefinitionItem term="Platforms">
+          {game.parent_platforms.map(({ platform }) => (
+            <Text key={platform.id}>{platform.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="Score">
+          <CriticScore score={game.metacritic} />
+        </DefinitionItem>
+        <DefinitionItem term="Genres">
+          {game.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="Publishers">
+          {game.publishers.map((publisher) => (
+            <Text key={publisher.id}>{publisher.name}</Text>
+          ))}
+        </DefinitionItem>
+      </SimpleGrid>
     </>
   );
 };
